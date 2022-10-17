9f07b53b38b9dd8d97995e24370d50ba6359406a
Silva-6 <silva6media@gmail.com>
2022-10-17 22:06:43 +0100

    Updated

--- 06-python-import_and_scope/employee.py
+++
@@ -1,8 +0,0 @@
-employee_name = "Mario"
-age = "55"
-title = "owner"
-
-def details():
-    print("Employee name is:  ", employee_name)
-    print("Employee age is: ", age)
-    print("Employee title is:  ", title)
